38ecb54548d6aadc41a5f2bd092c328c5bfb4e42
Kelvin Tsoi <tsoikelvin@gmail.com>
2025-03-16 13:55:44 -0700

    feat(users-node): add more queries by id and email to users resolver

--- services/users-node/src/resolvers/users.resolver.ts
+++ services/users-node/src/resolvers/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, ObjectType, Field, ID } from '@nestjs/graphql';
+import { Resolver, Query, ObjectType, Field, ID, Args } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
@@ -15,23 +15,37 @@ export class User {
   email!: string;
 }
 
+const users: User[] = [
+  {
+    id: '1',
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'johndoe@example.com',
+  },
+  {
+    id: '2',
+    firstName: 'Jane',
+    lastName: 'Doe',
+    email: 'janedoe@example.com',
+  },
+];
+
 @Resolver(() => User)
 export class UsersResolver {
   @Query(() => [User])
   getUsers(): User[] {
-    return [
-      {
-        id: '1',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-      },
-      {
-        id: '2',
-        firstName: 'Jane',
-        lastName: 'Doe',
-        email: 'janedoe@example.com',
-      },
-    ];
+    return users;
+  }
+
+  @Query(() => User, { nullable: true })
+  getUserById(@Args('id', { type: () => ID }) id: string): User | undefined {
+    return users.find((user) => user.id === id);
+  }
+
+  @Query(() => User, { nullable: true })
+  getUserByEmail(
+    @Args('email', { type: () => String }) email: string,
+  ): User | undefined {
+    return users.find((user) => user.email === email);
   }
 }
