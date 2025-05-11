2fa099d2aef4088de6cb4f78b8c8e59c7b0ffb16
Kelvin Tsoi <tsoikelvin@gmail.com>
2025-05-11 13:51:11 -0700

    feat(gateway): clean up index.ts file, add health checks, temporarily remove collections for refact

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -2,14 +2,33 @@ import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+const serviceConfig = {
+    users: { name: 'users', url: 'http://localhost:3000' },
+    products: { name: 'products', url: 'http://localhost:3001' },
+}
+
+async function checkServiceStatus(healthUrl: string, name: string): Promise<void> {
+    try {
+        const response = await fetch(healthUrl);
+        if (!response.ok) {
+          throw new Error(`⚠️ ${name} service is down`)
+        }
+        console.log(`✅ ${name} service is running at ${healthUrl}`);
+    } catch (error) {
+        console.error(`\`❌ ${name} error checking service ${healthUrl}:`, error);
+    }
+}
+
 async function startGateway() {
+  const services = Object.values(serviceConfig);
+
+  for (const service of services) {
+    await checkServiceStatus(service.url + '/health', service.name);
+  }
+
   const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
-      subgraphs: [
-        { name: 'users', url: 'http://localhost:3000/graphql' },
-        { name: 'products', url: 'http://localhost:3001/graphql' },
-        { name: 'collections', url: 'http://localhost:3002/graphql' },
-      ],
+      subgraphs: services.map(({ name, url }) => ({ name, url: url + '/graphql' })),
     }),
   });
 
